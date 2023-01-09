75c21f3342073c7e2af243cd62aab2e15710a6e1
Darien Sandifer <dariens@duck.com>
2023-01-08 21:17:02 -0600

    no message

--- stocks.py
+++ stocks.py
@@ -5,8 +5,10 @@ import matplotlib.pyplot as plt
 #play with these stock symbols for more comparisons -> https://finance.yahoo.com/lookup/index/
 companies = ['NVDA','F', 'MSFT','GOOG','TSLA']
 listofdf = []
+yourAPIKEY = ""
+
 for item in companies:
-    histprices = requests.get(f"https://financialmodelingprep.com/api/v3/historical-price-full/{item}?serietype=line&from=2020-03-12&to=2021-03-12&apikey=YOURAPIKEY")
+    histprices = requests.get(f"https://financialmodelingprep.com/api/v3/historical-price-full/{item}?serietype=line&from=2020-03-12&to=2021-03-12&apikey={yourAPIKEY}")
     histprices = histprices.json()
     print(histprices) 
 
